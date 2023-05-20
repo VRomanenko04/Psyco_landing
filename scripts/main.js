a9340ff2181fb061ec46f4c300135eedbf5fd8d1
VRomanenko04 <vovasan2004@gmail.com>
2023-05-21 01:56:17 +0300

    Блок "Отзывы" + стили + медиазапросы + правки

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,8 @@ document.querySelector('.first__btn').addEventListener('click', () => {
         title: 'Інструкція',
         icon: 'info',
         html: 
-        'Перейдіть за посиланням на одну з представлених соціальних мереж та напишіть на що ви бажаєте записатися.<br>' +
+        'Перейдіть за посиланням на одну з представлених соціальних мереж та напишіть на що ви бажаєте записатися. ' +
+        "Ми з вами зв'яжемось - запропонуємо можливі дати та проведемо смс консультацію.<br>" +
         '<a href="#"><img class="social__item me-2 mt-3" src="imgs/facebook icon.svg" alt="facebook"></a>' +
         '<a href="#"><img class="social__item mt-3" src="imgs/inst icon.svg" alt="instagram"></a>',
         confirmButtonColor: '#79afff',
@@ -22,7 +23,8 @@ document.querySelector('.second__btn').addEventListener('click', () => {
         title: 'Інструкція',
         icon: 'info',
         html: 
-        'Перейдіть за посиланням на одну з представлених соціальних мереж та напишіть на що ви бажаєте записатися.<br>' +
+        'Перейдіть за посиланням на одну з представлених соціальних мереж та напишіть на що ви бажаєте записатися. ' +
+        "Ми з вами зв'яжемось - запропонуємо можливі дати та проведемо смс консультацію.<br>" +
         '<a href="#"><img class="social__item me-2 mt-3" src="imgs/facebook icon.svg" alt="facebook"></a>' +
         '<a href="#"><img class="social__item mt-3" src="imgs/inst icon.svg" alt="instagram"></a>',
         confirmButtonColor: '#79afff',
@@ -39,7 +41,8 @@ document.querySelector('.third__btn').addEventListener('click', () => {
         title: 'Інструкція',
         icon: 'info',
         html: 
-        'Перейдіть за посиланням на одну з представлених соціальних мереж та напишіть на що ви бажаєте записатися.<br>' +
+        'Перейдіть за посиланням на одну з представлених соціальних мереж та напишіть на що ви бажаєте записатися. ' +
+        "Ми з вами зв'яжемось - запропонуємо можливі дати та проведемо смс консультацію.<br>" +
         '<a href="#"><img class="social__item me-2 mt-3" src="imgs/facebook icon.svg" alt="facebook"></a>' +
         '<a href="#"><img class="social__item mt-3" src="imgs/inst icon.svg" alt="instagram"></a>',
         confirmButtonColor: '#79afff',
